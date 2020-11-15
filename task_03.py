7bf4362c9cce5a9a393d06454f60328880040dc1
Sergey Emelyanov <karagabul@gmail.com>
2020-11-15 16:07:29 +0300

    Change task_03

--- task_03.py
+++ task_03.py
@@ -1,5 +1,10 @@
-def my_function():
-    pass
+def my_function(val_1, val_2):
+    if isinstance(val_1, int):
+        return [bool(v % val_1) for v in val_2]
+    elif isinstance(val_1, str):
+        return [val_1 in v for v in val_2]
+    else:
+        raise TypeError('val_1 must be string or int')
 
 
 my_function(10, [100, 200, 35, 40, 20])
